df24d159d24990abaeec4b1ad8336037ed9e8d30
msramalho <up201403027@fe.up.pt>
2019-03-21 18:04:01 +0000

    testing structure

---
+++ stuns/dispatcher.py
@@ -0,0 +1,8 @@
+class dispatcher():
+    """class used to interpret network file system paths and to dispatch them to the correct sensor parser"""
+    def __init__(self):
+        pass
+    
+    def handle_dir(self, dir):
+        """given a directory with user folders, find all .txt sensor acquisitions"""
+        pass
---
+++ stuns/stuns.py
@@ -0,0 +1,7 @@
+from sensors.sensor import sensor
+
+
+# start_point
+s = sensor()
+
+print(s)
