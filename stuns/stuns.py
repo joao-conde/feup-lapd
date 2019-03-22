b53257cea24ebf6374d55e792824a71bab5032c3
msramalho <up201403027@fe.up.pt>
2019-03-22 18:22:44 +0000

    initial parser setup

--- stuns/stuns.py
+++ stuns/stuns.py
@@ -1,7 +1,20 @@
+from sys import argv
+import argparse
 from sensors.sensor import sensor
 
+def structure_the_unstructured(path):
+    print(path)
+    s = sensor()
+    print(s)
 
-# start_point
-s = sensor()
+def parse_args():
+    parser = argparse.ArgumentParser(description="Structuring The UNStructured, a command line tool for mobile sensor data. Network file system files into mongoDB")
+    group_settings = parser.add_argument_group('global settings')
+    group_settings.add_argument('-p','--path', help='The source folder where the user folder are contained', required=True)
+    return vars(parser.parse_args())
 
-print(s)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    structure_the_unstructured(args["path"])
