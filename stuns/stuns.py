bffee38fa848751201fd4a183ab1c43d7a0f40cf
msramalho <up201403027@fe.up.pt>
2019-03-22 18:28:22 +0000

    docs

--- stuns/stuns.py
+++ stuns/stuns.py
@@ -8,6 +8,9 @@ def structure_the_unstructured(path):
     print(s)
 
 def parse_args():
+    """Uses argparse module to create a pretty CLI interface that has the -h by default and that helps the user understand the arguments and their usage
+    Returns a dict of "argname":"value"
+    """
     parser = argparse.ArgumentParser(description="Structuring The UNStructured, a command line tool for mobile sensor data. Network file system files into mongoDB")
     group_settings = parser.add_argument_group('global settings')
     group_settings.add_argument('-p','--path', help='The source folder where the user folder are contained', required=True)
