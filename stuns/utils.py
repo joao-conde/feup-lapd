ed30f0177a7065139587bd7a36dd37f05d225d34
msramalho <up201403027@fe.up.pt>
2019-03-23 00:28:35 +0000

    some utils

--- stuns/utils.py
+++ stuns/utils.py
@@ -1,1 +1,11 @@
+import os
+import sys
+import glob
+
+
 def sum(a, b): return a + b
+
+def get_all_files_recursively(root_dir):
+    """returns a list of all the files inside the given root_dir, recursively"""
+    # TODO: maybe filter for .txt only or to ignore xml
+    return filter(os.path.isfile, glob.iglob(root_dir + '**/**', recursive=True))
