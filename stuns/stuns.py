36b25eb3d8bfc2855d7dae22960880d08d620137
msramalho <up201403027@fe.up.pt>
2019-03-23 00:28:45 +0000

    argparse

--- stuns/stuns.py
+++ stuns/stuns.py
@@ -1,11 +1,19 @@
 from sys import argv
 import argparse
 from sensors.sensor import sensor
+from dispatcher import dispatcher
+from utils import get_all_files_recursively
+
 
 def structure_the_unstructured(path):
-    print(path)
+    print("path = ", path)
+    d = dispatcher()
+    for f in get_all_files_recursively(path):
+        d.dispatch(f)
+
+    print(files[0])
     s = sensor()
-    print(s)
+
 
 def parse_args():
     """Uses argparse module to create a pretty CLI interface that has the -h by default and that helps the user understand the arguments and their usage
@@ -13,9 +21,8 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description="Structuring The UNStructured, a command line tool for mobile sensor data. Network file system files into mongoDB")
     group_settings = parser.add_argument_group('global settings')
-    group_settings.add_argument('-p','--path', help='The source folder where the user folder are contained', required=True)
+    group_settings.add_argument('-p', '--path', help='The source folder where the user folder are contained', required=True)
     return vars(parser.parse_args())
-
 
 
 if __name__ == "__main__":
