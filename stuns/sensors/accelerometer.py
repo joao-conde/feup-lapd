33a4e722b3f7252e219dec3fe2a4f2b747a7199a
msramalho <up201403027@fe.up.pt>
2019-03-24 01:25:32 +0000

    accelerometer and gyroscope input file structure

--- stuns/sensors/accelerometer.py
+++ stuns/sensors/accelerometer.py
@@ -4,5 +4,6 @@ from .sensor import sensor
 class accelerometer(sensor):
     """Sensor for accelerometer data, structure is `timestamp, x, y, z, precision`"""
     filter = ".*Accelerometer\.txt"
+    columns = ['timestamp', 'x,', 'y,', 'z,', 'precision']
     def __init__(self, file):
         super().__init__(file)
--- stuns/sensors/gyroscope.py
+++ stuns/sensors/gyroscope.py
@@ -4,5 +4,7 @@ from .sensor import sensor
 class gyroscope(sensor):
     """Sensor for gyroscope data, structure is `timestamp, x, y, z, precision`"""
     filter = ".*Gyroscope\.txt"
+    columns = {'timestamp', 'x,', 'y,', 'z,', 'precision'}
     def __init__(self, file):
         super().__init__(file)
+    
