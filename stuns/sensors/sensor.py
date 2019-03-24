137fd44b48978d13b6f9c1a608495e070b36acf2
msramalho <up201403027@fe.up.pt>
2019-03-24 01:25:12 +0000

    sensor assertions + started using pandas

--- stuns/sensors/sensor.py
+++ stuns/sensors/sensor.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
 class sensor:
     """Abstract parent class that defines the method every sensor parser should implement"""
 
     def __init__(self, file):
-        pass
+        assert self.__class__.filter is not None, "Each sensor should have a static value for 'filter' to identify its files"
+        assert self.__class__.columns is not None, "Each sensor should have a static value for 'columns' for the csv-like data columns in the files"
+        self.file = file
+        self.df = pd.read_csv(self.file, delimiter=",") #, usecols=self.__class__.columns
+        print(self.df)
