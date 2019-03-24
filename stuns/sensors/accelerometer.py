8b82d68b99ab0a92e9a31ed6f9b87c2b56bbf72e
msramalho <up201403027@fe.up.pt>
2019-03-24 12:46:43 +0000

    preprocessing timestamps

--- stuns/sensors/accelerometer.py
+++ stuns/sensors/accelerometer.py
@@ -1,9 +1,14 @@
+import pandas as pd
 from .sensor import sensor
-
 
 class accelerometer(sensor):
     """Sensor for accelerometer data, structure is `timestamp, x, y, z, precision`"""
     filter = ".*Accelerometer\.txt"
     columns = ['timestamp', 'x,', 'y,', 'z,', 'precision']
+
     def __init__(self, file, details):
         super().__init__(file, details)
+
+    
+    def preprocessing_dataframe(self):
+        self.df["timestamp"] = pd.to_datetime(self.df["timestamp"], unit="ms")
--- stuns/sensors/gyroscope.py
+++ stuns/sensors/gyroscope.py
@@ -5,5 +5,9 @@ class gyroscope(sensor):
     """Sensor for gyroscope data, structure is `timestamp, x, y, z, precision`"""
     filter = ".*Gyroscope\.txt"
     columns = ['timestamp', 'x,', 'y,', 'z,', 'precision']
+
     def __init__(self, file, details):
         super().__init__(file, details)
+
+    def preprocessing_dataframe(self):
+        self.df["timestamp"] = pd.to_datetime(self.df["timestamp"], unit="ms")
