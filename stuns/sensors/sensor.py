b9ff9f8f7cd687946f290f3ce3dba77dca932962
msramalho <up201403027@fe.up.pt>
2019-03-30 19:19:25 +0000

    improved sensor class workflow

--- stuns/sensors/sensor.py
+++ stuns/sensors/sensor.py
@@ -13,18 +13,30 @@ class sensor:
         self.df = pd.read_csv(self.file, delimiter=",", names=self.__class__.columns)
 
     def parse(self):
+        """Main method that handles a sensor file, performs preprocessing operation, extracts metrics and imports the data into the MongoDB instance"""
         self.preprocessing_dataframe()
-        list(map(self.parse_line, self.df.iterrows()))
+        self.df.apply(self.parse_row, axis=1)
+        metrics = self.extract_metrics()
+        self.insert_into_db()
+        return metrics
+
+    def insert_into_db(self):
+        """Performs database connection operations and inserts the dataframe data into it"""
         # with MongoClient('mongodb://localhost:27017/') as client:
         # db = client["database_name"]
         # collection = db["collection_name"]
-        # collection.insert_many(map(self.parse_line, df.iterrows()))
+        # collection.insert_many(map(self.parse_row, df.iterrows()))
+        pass
+
+    def extract_metrics(self):
+        """Extract metrics specific to this datafile"""
+        return dict()
 
     def preprocessing_dataframe(self):
         """This method can be extended by child classes to do things like bulk conversion of dataframe column from text to date object and other preprocessing operations done before bulk inserting into the database"""
         pass
 
-    def parse_line(index, row):
+    def parse_row(self, row):
         """Should convert a row into a dict for insertion in MongoDB"""
-        print(row)
-        exit()
+        pass
+        # print(row);exit()
