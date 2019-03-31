18e136db287be151c680a34d9945347cceb0f113
msramalho <up201403027@fe.up.pt>
2019-03-31 17:14:48 +0100

    refactored sensor and implemented default metrics

--- stuns/sensors/sensor.py
+++ stuns/sensors/sensor.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import pymongo
-
+import json
+import time
+from utils import hash_file
 
 class sensor:
     """Abstract parent class that defines the method every sensor parser should implement"""
@@ -12,12 +14,26 @@ class sensor:
         self.details = details
         self.df = pd.read_csv(self.file, delimiter=",", names=self.__class__.columns)
 
-    def parse(self):
+    def parse(self, old_metrics=None):
         """Main method that handles a sensor file, performs preprocessing operation, extracts metrics and imports the data into the MongoDB instance"""
         self.preprocessing_dataframe()
-        self.df.apply(self.parse_row, axis=1)
-        metrics = self.extract_metrics()
+        # self.df.apply(self.process_row, axis=1)
+        metrics = self.extract_metrics(old_metrics)
         self.insert_into_db()
+        return metrics
+
+    def preprocessing_dataframe(self):
+        """This method can be extended by child classes to do things like bulk conversion of dataframe column from text to date object and other preprocessing operations done before bulk inserting into the database"""
+        pass
+
+    def extract_metrics(self, _metrics=None):
+        """Extract metrics specific to this datafile. Can be extended by child classes that call it for the default metrics"""
+        metrics = _metrics if _metrics else dict()
+        metrics["filename"] = self.file
+        metrics["structured_at"] = time.time()
+        metrics["rows"] = len(self.df)
+        metrics["hash"] = hash_file(self.file)
+        metrics.update(self.details)
         return metrics
 
     def insert_into_db(self):
@@ -25,18 +41,8 @@ class sensor:
         # with MongoClient('mongodb://localhost:27017/') as client:
         # db = client["database_name"]
         # collection = db["collection_name"]
-        # collection.insert_many(map(self.parse_row, df.iterrows()))
+        # collection.insert_many(map(self.row_to_dict, self.df.iterrows()))
+        # alternative that generates the whole JSON
+        records = json.loads(self.df.T.to_json()).values()
+        # collection.insert(records)
         pass
-
-    def extract_metrics(self):
-        """Extract metrics specific to this datafile"""
-        return dict()
-
-    def preprocessing_dataframe(self):
-        """This method can be extended by child classes to do things like bulk conversion of dataframe column from text to date object and other preprocessing operations done before bulk inserting into the database"""
-        pass
-
-    def parse_row(self, row):
-        """Should convert a row into a dict for insertion in MongoDB"""
-        pass
-        # print(row);exit()
