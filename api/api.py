a6ee44c777bd65d25beda2a532377feb63236da3
cyrilico <jpdamas15@gmail.com>
2019-05-06 10:41:44 +0100

    API extension now communicates with DB

--- api/api.py
+++ api/api.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from pymongo import MongoClient
+
 app = Flask(__name__)
+mongo_client = MongoClient(host='demdata_mongodb')
+db = mongo_client.demdata_db
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return str(db.datasets.count_documents({}))
+
+@app.route('/cenas')
+def hello():
+    return 'hello world'
 
 
 app.run(debug=True, host='0.0.0.0', port=5050)
