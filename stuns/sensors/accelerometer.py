0bc4662e508d4341ff441f6259f10ed8f31592c2
msramalho <up201403027@fe.up.pt>
2019-05-11 15:02:43 +0100

    removed unnecessary

--- stuns/sensors/accelerometer.py
+++ stuns/sensors/accelerometer.py
@@ -13,8 +13,3 @@ class accelerometer(sensor):
     def __init__(self, file, metrics_args):
         super().__init__(file, metrics_args)
 
-    def extract_metrics(self, metrics={}):
-        metrics = super().extract_metrics(metrics)
-        
-        return metrics
-
