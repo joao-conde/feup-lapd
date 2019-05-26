9c037f8be51796098d15a5d81438e0dda0a36604
msramalho <up201403027@fe.up.pt>
2019-05-26 15:47:12 +0100

    Magnetometer

---
+++ stuns/sensors/magnetometer.py
@@ -0,0 +1,13 @@
+from .sensor import sensor
+import pandas as pd
+
+
+class magnetometer(sensor):
+    """Sensor for magnetometer data, structure is `timestamp, x, y, z, precision`"""
+    filter = ".*Magnetometer\.txt"
+    columns = ['timestamp', 'x', 'y', 'z', 'precision']
+    name = "MADGNETOMETER"
+    inercial = True
+
+    def __init__(self, file, metrics_args):
+        super().__init__(file, metrics_args)
