517ff8410faf0a088c2822182faedb5b93ece016
msramalho <up201403027@fe.up.pt>
2019-05-26 16:05:08 +0100

    typo

--- stuns/sensors/magnetometer.py
+++ stuns/sensors/magnetometer.py
@@ -6,7 +6,7 @@ class magnetometer(sensor):
     """Sensor for magnetometer data, structure is `timestamp, x, y, z, precision`"""
     filter = ".*Magnetometer\.txt"
     columns = ['timestamp', 'x', 'y', 'z', 'precision']
-    name = "MADGNETOMETER"
+    name = "MAGNETOMETER"
     inercial = True
 
     def __init__(self, file, metrics_args):
